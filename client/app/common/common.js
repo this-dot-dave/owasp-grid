24592a5ba77bb95f01482341308ab4e5734b6499
Dave <return.this.dave@icloud.com>
2017-06-06 20:34:43 -0400

    remove unused User component

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,13 +1,11 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Hero from './hero/hero';
-import User from './user/user';
 import Footer from './footer/footer';
 
 let commonModule = angular.module('app.common', [
   Navbar,
   Hero,
-  User,
   Footer
 ])
   
